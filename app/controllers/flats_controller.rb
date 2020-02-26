ac9d8c3adcc3c951694159496dae1a16088d5d6d
Neto Concon <concon.neto2@gmail.com>
2020-02-26 20:52:15 -0300

    add geocode to index action on controller

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -4,7 +4,14 @@ class FlatsController < ApplicationController
   # GET /flats
   # GET /flats.json
   def index
-    @flats = Flat.all
+    @flats = Flat.geocoded #returns flats with coordinates
+
+    @markers = @flats.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+      }
+    end
   end
 
   # GET /flats/1
