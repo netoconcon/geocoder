3b4d23f77c2adb66418a7832d1e104520f0d8eb3
Neto Concon <concon.neto2@gmail.com>
2020-02-26 21:35:58 -0300

    add pop up with flat info after click map marker

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -9,7 +9,9 @@ class FlatsController < ApplicationController
     @markers = @flats.map do |flat|
       {
         lat: flat.latitude,
-        lng: flat.longitude
+        lng: flat.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { flat: flat })
+        # show info on map marker after clicked
       }
     end
   end
