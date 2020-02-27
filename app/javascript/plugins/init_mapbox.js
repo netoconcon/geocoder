353fe3273a9f207a154659317af0932932ea1bee
Neto Concon <concon.neto2@gmail.com>
2020-02-26 21:21:09 -0300

    add initial zoom to the map marker

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -17,6 +17,20 @@ const initMapbox = () => {
         .setLngLat([ marker.lng, marker.lat ])
         .addTo(map);
       });
+
+      const fitMapToMarkers = (map, markers) => {
+        const bounds = new mapboxgl.LngLatBounds();
+        markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+        map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+      };
+
+      if (mapElement) {
+        // [ ... ]
+        fitMapToMarkers(map, markers);
+      }
+
+
+
   }
 };
 
